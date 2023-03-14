8f3426077337e11f20ae358ca570014439bee694
Pavel Romanov <pavel.romanov@saritasa.com>
2023-03-14 10:56:17 +0600

    Improved ESLint configuration

--- libs/immersive/src/lib/main-light.ts
+++ libs/immersive/src/lib/main-light.ts
@@ -3,6 +3,10 @@ import { Color3, HemisphericLight, Scene, Vector3 } from '@babylonjs/core';
 /** Main light of the scene. */
 export class MainLight {
 
+  /**
+   * Creates main light of the scene.
+   * @param scene Scene.
+   */
   public static create(scene: Scene): void {
     const hemiLight = new HemisphericLight('mainLight', new Vector3(0, 2, 0), scene);
     hemiLight.intensity = 0.6;
